39d4aaa3b4b28e66a87c8678f46c77b96d4da5fc
Tony McGee <tmcgee.au@gmail.com>
2024-12-21 01:33:33 +1000

    Day 8 part 2 completed

--- src/08.rs
+++ src/08.rs
@@ -28,7 +28,7 @@ impl AntennaMap {
         self.chars.iter().filter(|(_, c)| **c == ch).map(|(p, _)| *p).collect()
     }
 
-    fn find_antinodes(&self, ch: char) -> HashSet<Point32> {
+    fn find_antinodes(&self, ch: char, iter: u16) -> HashSet<Point32> {
         let mut antinodes = HashSet::new();
 
         let freq = self.find_freq(ch);
@@ -41,11 +41,25 @@ impl AntennaMap {
             let dx = f2.x - f1.x;
             let dy = f2.y - f1.y;
 
-            let p1 = Point32 { x: f1.x - dx, y: f1.y - dy };
-            let p2 = Point32 { x: f2.x + dx, y: f2.y + dy };
+            let mut p1_iter = iter;
+            let mut p1 = Point32 { x: f1.x - dx, y: f1.y - dy };
+            while p1_iter > 0 && self.valid_pos(p1) {
+                antinodes.insert(p1);
+                p1 = Point32 { x: p1.x - dx, y: p1.y - dy };
+                p1_iter -= 1;
+            }
 
-            if self.valid_pos(p1) { antinodes.insert(p1); }
-            if self.valid_pos(p2) { antinodes.insert(p2); }
+            let mut p2_iter = iter;
+            let mut p2 = Point32 { x: f2.x + dx, y: f2.y + dy };
+            while p2_iter > 0 && self.valid_pos(p2) {
+                antinodes.insert(p2);
+                p2 = Point32 { x: p2.x + dx, y: p2.y + dy };
+                p2_iter -= 1;
+            }
+            if iter > 1 {
+                antinodes.insert(*f1);
+                antinodes.insert(*f2);
+            }
 
             //println!("f1=[{},{}]  f2=[{},{}]  d=[{},{}]", f1.x, f1.y, f2.x, f2.y, dx, dy);
             //println!("p1=[{},{}]  p2=[{},{}]", p1.x, p1.y, p2.x, p2.y);
@@ -83,22 +97,29 @@ fn default_input() -> &'static str {
 pub fn part1() -> String {
     let amap = AntennaMap::from(default_input());
 
-    let mut hmap = HashSet::new();
+    let mut hset = HashSet::new();
 
     for freq in amap.all_freq() {
-        for anti in amap.find_antinodes(freq) {
-            hmap.insert(anti);
+        for anti in amap.find_antinodes(freq, 1) {
+            hset.insert(anti);
         }
     }
 
-    hmap.len().to_string()
+    hset.len().to_string()
 }
 
 pub fn part2() -> String {
-    let _amap = AntennaMap::from(include_input!(08test1));
+    let amap = AntennaMap::from(default_input());
 
-    // TODO: Implement Part 2
-    String::from("")
+    let mut hset = HashSet::new();
+
+    for freq in amap.all_freq() {
+        for anti in amap.find_antinodes(freq, 1000) {
+            hset.insert(anti);
+        }
+    }
+
+    hset.len().to_string()
 }
 
 fn main() {
@@ -111,7 +132,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_valid_pos() {
+    fn valid_pos() {
         let amap = AntennaMap::from(include_input!(08test1));
         assert_eq!(amap.width, 10);
         assert_eq!(amap.height, 10);
@@ -127,13 +148,13 @@ mod tests {
     }
 
     #[test]
-    fn test_all_freq() {
+    fn all_freq() {
         let amap = AntennaMap::from(include_input!(08test1));
         assert_eq!(amap.all_freq(), vec!['#','a']);
     }
 
     #[test]    
-    fn test_find_freq() {
+    fn find_freq() {
         let amap = AntennaMap::from(include_input!(08test1));
         let freq = amap.find_freq('a');
         assert_eq!(freq.len(), 3);
@@ -143,14 +164,45 @@ mod tests {
     }
 
     #[test]
-    fn test_find_antinodes() {
+    fn find_antinodes_08test1() {
         let amap = AntennaMap::from(include_input!(08test1));
-        let antinodes = amap.find_antinodes('a');
+        let antinodes = amap.find_antinodes('a', 1);
         assert_eq!(antinodes.len(), 4);
         assert!(antinodes.contains(&Point32 { x: 3, y: 1 }));
         assert!(antinodes.contains(&Point32 { x: 0, y: 2 }));
         assert!(antinodes.contains(&Point32 { x: 6, y: 7 }));
         assert!(antinodes.contains(&Point32 { x: 2, y: 6 }));
+    }
+
+    #[test]
+    fn find_antinodes_08test2() {
+        let amap = AntennaMap::from(include_input!(08test2));
+        let antinodes = amap.find_antinodes('T', 1000);
+        assert_eq!(antinodes.len(), 9);
+        assert!(antinodes.contains(&Point32 { x: 5, y: 0 }));
+        assert!(antinodes.contains(&Point32 { x: 6, y: 2 }));
+        assert!(antinodes.contains(&Point32 { x: 9, y: 3 }));
+        assert!(antinodes.contains(&Point32 { x: 2, y: 4 }));
+        assert!(antinodes.contains(&Point32 { x: 3, y: 6 }));
+        assert!(antinodes.contains(&Point32 { x: 4, y: 8 }));
+        assert!(antinodes.contains(&Point32 { x: 0, y: 0 }));
+        assert!(antinodes.contains(&Point32 { x: 3, y: 1 }));
+        assert!(antinodes.contains(&Point32 { x: 1, y: 2 }));
+        
+    }
+
+    #[test]
+    fn find_antinodes_08test3() {
+        let amap = AntennaMap::from(include_input!(08test3));
+        let mut hset = HashSet::new();
+
+        for freq in amap.all_freq() {
+            for anti in amap.find_antinodes(freq, 1000) {
+                hset.insert(anti);
+            }
+        }
+
+        assert_eq!(hset.len(), 34);
     }
 
     #[test]
@@ -160,6 +212,6 @@ mod tests {
 
     #[test]
     fn solve_part2() {
-        assert_eq!(part2(), "zz");
+        assert_eq!(part2(), "1259");
     }
 }
